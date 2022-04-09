cb9b4b679e04252e0863793dfbd118c11520c164
timurmametyev <tmametev@b2broker.com>
2022-04-09 18:37:23 +0300

    Move application run to bootstrap config file

--- api/public/index.php
+++ api/public/index.php
@@ -2,17 +2,17 @@
 
 declare(strict_types=1);
 
-use Slim\Factory\AppFactory;
+use Psr\Container\ContainerInterface;
+use Slim\App;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+/** @var ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
-$app = AppFactory::createFromContainer($container);
-
-(require __DIR__ . '/../config/middleware.php')($app, $container);
-(require __DIR__ . '/../config/routes.php')($app);
+/** @var App $app */
+$app = (require __DIR__ . '/../config/bootstrap.php')($container);
 
 $app->run();
